1d95ebe0826b37cf43e4f73a8d0214fe92ff833f
SihabAnik-Dev <sihabanik1@gmail.com>
2023-03-07 02:39:17 +0600

    file creation

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+
+
+$(".Services-inner").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    nextArrow:".next",
+    prevArrow:".prev",
+  });
+
+
+$('.counter').counterUp({
+  delay: 10,
+  time: 1000
+});
